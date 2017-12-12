a5285e86c7b85c730dc9f9857a15bc005dc59691
Tomasz Kasprzyk <kasptom@gmail.com>
2017-12-12 12:47:07 +0100

    Clean up

--- src/lstm_network.py
+++ src/lstm_network.py
@@ -32,17 +32,19 @@ print('Loading data...')
 
 print('x_train shape:', x_train.shape)
 print('x_test shape:', x_test.shape)
-########################################################################################################################
+
 print('Build model...')
 embedding_vecor_length = 64
 model = Sequential()
-model.add(Embedding(batch_input_shape=(None, max_features), batch_size=batch_size, input_dim=max_features, output_dim=128))
+model.add(
+    Embedding(input_dim=max_features, batch_input_shape=(None, max_features), batch_size=batch_size, output_dim=128)
+)
 model.add(LSTM(128, dropout=0.2, recurrent_dropout=0.2))
 model.add(Dense(1, activation='sigmoid'))
 
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 print(model.summary())
-########################################################################################################################
+
 print('Train...')
 model.fit(x_train, y_train,
           batch_size=batch_size,

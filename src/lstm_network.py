bf766cbcb3041235d0bd7c8205dd6cd45a71609b
Tomasz Kasprzyk <kasptom@gmail.com>
2018-01-26 02:14:19 +0100

    Fix data generation

--- src/lstm_network.py
+++ src/lstm_network.py
@@ -13,8 +13,8 @@ from src import configuration
 from src.configuration import WORD_NUMERIC_VECTOR_SIZE, EPOCHS_NUMBER, DROPOUT, RECURRENT_DROPOUT, \
     TIME_STEPS, get_network_model_snapshot, DATA_SET, get_csv_log_file_name
 from src.preprocessing.document_as_w2v_groups import ensure_word_numeric_representation_created
-from src.utils.data_generator import get_test_generator, get_train_samples_count, \
-    get_test_samples_count, get_train_generator
+from src.utils.data_generator import DataGenerator
+
 from src.utils.get_file import create_file_and_folders_if_not_exist
 
 """
@@ -32,6 +32,8 @@ print('Loading data...')
 start = time.time()
 
 ensure_word_numeric_representation_created()
+
+data_generator = DataGenerator()
 
 end = time.time()
 print("time elapsed: ", end - start, " seconds")
@@ -59,12 +61,12 @@ print("time elapsed: ", end - start, " seconds")
 
 print('Train...')
 start = time.time()
-model.fit_generator(get_train_generator(),
+model.fit_generator(data_generator.get_train_generator(),
                     epochs=EPOCHS_NUMBER,
-                    validation_data=get_test_generator(),
-                    samples_per_epoch=get_train_samples_count() // TIME_STEPS,
+                    validation_data=data_generator.get_test_generator(),
+                    samples_per_epoch=data_generator.get_train_samples_count() // TIME_STEPS,
                     callbacks=callbacks,
-                    validation_steps=get_test_samples_count() // TIME_STEPS)
+                    validation_steps=data_generator.get_test_samples_count() // TIME_STEPS)
 
 # score, acc = model.evaluate(x_test, y_test, batch_size=32)
 
--- src/utils/data_generator.py
+++ src/utils/data_generator.py
@@ -8,51 +8,49 @@ prev_val_counter = 0
 files_counter = 0
 
 
-def get_train_samples_count():
-    _data_path = get_vector_words_directory(DATA_SET['label'])
-    _files = [f for f in listdir(_data_path)]
-    return len(_files) * ((100 - TEST_DATA_PERCENTAGE) / 100)
+class DataGenerator:
+    def __init__(self):
+        self.data_path = get_vector_words_directory(DATA_SET['label'])
+        self.files = [f for f in listdir(self.data_path)]
+        self.labels = np.load(get_vector_labels_file_name(DATA_SET['label']))
 
+        self.train_samples_count = int(round(len(self.files) * (100 - TEST_DATA_PERCENTAGE) / 100, 0))
+        self.test_samples_count = len(self.files) - self.train_samples_count
 
-def get_test_samples_count():
-    _data_path = get_vector_words_directory(DATA_SET['label'])
-    _files = [f for f in listdir(_data_path)]
-    return len(_files) * TEST_DATA_PERCENTAGE / 100
+        self.train_files = self.files[:self.train_samples_count]
+        self.train_labels = self.labels[:self.train_samples_count]
 
+        self.test_files = self.files[self.train_samples_count:]
+        self.test_labels = self.labels[self.train_samples_count:]
 
-def get_train_generator(batch_size=DATA_SET['batch_size']):
-    data_path = get_vector_words_directory(DATA_SET['label'])
-    files = [f for f in listdir(data_path)]
-    labels = np.load(get_vector_labels_file_name(DATA_SET['label']))
+    def get_train_samples_count(self):
+        return self.train_samples_count
 
-    global prev_val_counter
-    global files_counter
+    def get_test_samples_count(self):
+        return self.test_samples_count
 
-    while files_counter < len(files) * ((100 - TEST_DATA_PERCENTAGE) / 100):
-        x = []
-        y = []
-        files_counter += batch_size
-        for idx in range(prev_val_counter, files_counter):
-            x.append(np.load(data_path + "/" + files[idx]))
-            y.append(np.array(labels[idx]))
-        prev_val_counter = files_counter
-        yield (np.array(x), np.array(y))
+    def get_train_generator(self, batch_size=DATA_SET['batch_size']):
+        idx = 0
+        while True:
+            x = []
+            y = []
+            counter = 0
+            while counter < batch_size:
+                x.append(np.load(self.data_path + "/" + self.train_files[idx]))
+                y.append(np.array(self.train_labels[idx]))
+                idx = (idx + 1) % self.train_samples_count
+                counter += 1
+            yield (np.array(x), np.array(y))
 
-
-def get_test_generator(batch_size=DATA_SET['batch_size']):
-    data_path = get_vector_words_directory(DATA_SET['label'])
-    files = [f for f in listdir(data_path)]
-    labels = np.load(get_vector_labels_file_name(DATA_SET['label']))
-
-    global prev_val_counter
-    global files_counter
-
-    while files_counter < len(files):
-        x = []
-        y = []
-        files_counter += batch_size
-        for idx in range(prev_val_counter, files_counter):
-            x.append(np.load(data_path + "/" + files[idx]))
-            y.append(np.array(labels[idx]))
-        prev_val_counter = files_counter
-        yield (np.array(x), np.array(y))
+    def get_test_generator(self, batch_size=DATA_SET['batch_size']):
+        idx = 0
+        while True:
+            x = []
+            y = []
+            counter = 0
+            while counter < batch_size:
+                x.append(np.load(self.data_path + "/" + self.test_files[idx]))
+                y.append(np.array(self.test_labels[idx]))
+                idx = (idx + 1) % self.test_samples_count
+                counter += 1
+            yield (np.array(x), np.array(y))

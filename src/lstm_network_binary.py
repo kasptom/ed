67939c3373201a56725ee25ad7df260061f045cb
Tomasz Kasprzyk <kasptom@gmail.com>
2018-01-28 19:28:49 +0100

    Add demo for binary classification

---
+++ src/lstm_network_binary.py
@@ -0,0 +1,16 @@
+from keras import Sequential
+from keras.layers import LSTM, Dense
+
+from src.configuration import TIME_STEPS, RECURRENT_DROPOUT, WORD_NUMERIC_VECTOR_SIZE, DROPOUT
+from src.lstm_net import LstmNet
+
+
+class LstmBinaryNet(LstmNet):
+    def build_model(self):
+        model = Sequential()
+        model.add(
+            LSTM(200, input_shape=(TIME_STEPS, WORD_NUMERIC_VECTOR_SIZE), dropout=DROPOUT,
+                 recurrent_dropout=RECURRENT_DROPOUT))
+        model.add(Dense(1, activation='sigmoid'))
+        model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+        return model
---
+++ src/train_network.py
@@ -0,0 +1,4 @@
+from src.lstm_network_binary import LstmBinaryNet
+
+lstm = LstmBinaryNet()
+lstm.train_network()

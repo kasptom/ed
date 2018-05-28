6958000575063e531cda236e557e8c3ab5442f0b
Tomasz Kasprzyk <kasptom@gmail.com>
2018-05-28 21:46:52 +0200

    Update bat script ;)

--- install_tensorflow_gpu_win64.bat
+++ install_tensorflow_gpu_win64.bat
@@ -1,1 +1,11 @@
-pip install --upgrade https://storage.googleapis.com/tensorflow/windows/gpu/tensorflow_gpu-0.12.1-cp35-cp35m-win_amd64.whl
+#pip install --upgrade https://storage.googleapis.com/tensorflow/windows/gpu/tensorflow_gpu-0.12.1-cp35-cp35m-win_amd64.whl
+# Sounds good does not work anymore
+# See https://www.tensorflow.org/install/install_windows and install it with Anaconda
+# Checked configuration:
+# -conda 4.4.10
+# -tensorflow-gpu 1.8.0
+# -tensorboard 1.8.0 (installed along with the one above)
+# -GTX 770, driver version: 397.93
+# -cuda: 9.0.176_win10
+# -cudnn: 9.0-windows10-x64-v7.0.5
+
